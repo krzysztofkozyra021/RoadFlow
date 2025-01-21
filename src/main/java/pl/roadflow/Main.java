9d3256d258c351c6bdf4c0628e94c7e7cb8d5a27
Tomasz Rebizant <tomasz.rebizant@studenci.collegiumwitelona.pl>
2025-01-21 16:07:16 +0100

    Add content to change menu

--- src/main/java/pl/roadflow/Main.java
+++ src/main/java/pl/roadflow/Main.java
@@ -1,11 +1,13 @@
 package src.main.java.pl.roadflow;
 
+import src.main.java.pl.roadflow.ui.screens.CarChangeScreen;
 import src.main.java.pl.roadflow.ui.screens.GameScreen;
 import src.main.java.pl.roadflow.ui.screens.StartingMenu;
 
 public class Main {
     public static void main(String[] args) {
-        StartingMenu startingMenu = new StartingMenu();
+        CarChangeScreen carChangeScreen = new CarChangeScreen();
+        //StartingMenu startingMenu = new StartingMenu();
        // GameScreen gameScreen = new GameScreen();
     }
 }
--- src/main/java/pl/roadflow/ui/screens/CarChangeScreen.java
+++ src/main/java/pl/roadflow/ui/screens/CarChangeScreen.java
@@ -2,16 +2,48 @@ package src.main.java.pl.roadflow.ui.screens;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 public class CarChangeScreen extends JFrame {
+    HashMap<String, String> carNames = new HashMap<String, String>();
     public CarChangeScreen() {
         setTitle("Car Change");
-        setSize(800, 600);
+        setSize(600, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
+
+        BackgroundPanel backgroundPanel = new BackgroundPanel();
+        backgroundPanel.setLayout(new GridLayout(5, 4));
+        add(backgroundPanel);
+
+        addPanels(backgroundPanel);
+
         setVisible(true);
-        GridLayout layout = new GridLayout(5, 4);
-        setLayout(layout);
+    }
+
+    private void addPanels(JPanel backgroundPanel) {
+        for (int i = 0; i < 20; i++) {
+            JPanel panel = new JPanel();
+            panel.setName("car" + (i+1));
+            panel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            panel.setBackground(new Color(0, 0, 0, 0));
+            backgroundPanel.add(panel);
+        }
+    }
+
+    private static class BackgroundPanel extends JPanel {
+        private Image backgroundImage;
+
+        public BackgroundPanel() {
+            ImageIcon icon = new ImageIcon("src/main/java/pl/roadflow/assets/cars/file.png");
+            backgroundImage = icon.getImage();
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
     }
 }
