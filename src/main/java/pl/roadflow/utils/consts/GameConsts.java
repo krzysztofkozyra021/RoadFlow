255115e74b17d2388208e552dc90918bc742f9c5
Cysiuu <cysiuu021@protonmail.com>
2025-01-25 11:36:39 +0100

    Map tiles fix, change map name

--- src/main/java/pl/roadflow/utils/consts/GameConsts.java
+++ src/main/java/pl/roadflow/utils/consts/GameConsts.java
@@ -1,7 +1,7 @@
 package src.main.java.pl.roadflow.utils.consts;
 
 public class GameConsts {
-    public static String MAP_FILE_PATH = "src/main/java/pl/roadflow/assets/maps/de_map.txt";
+    public static String MAP_FILE_PATH = "src/main/java/pl/roadflow/assets/maps/de_lake.txt";
     public static String CAR_FILE_PATH = "src/main/java/pl/roadflow/assets/cars/car5_red.png";
     public static int DEBUG_MODE = 0; // 1 - on, 0 - off
 }
